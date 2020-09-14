aa9ea103cd2e088c91ed5c671c7da42f6b336937
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-09-14 19:18:06 -0300

    Updates

---
+++ precache-manifest.c4c00477bca938e8a134233b32d1ffc4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06f2afc6efbf1383f1bbb7ddff6114d",
+    "url": "/mathieubouhelier/shopping-online.git/index.html"
+  },
+  {
+    "revision": "9efecb77677f5b68e824",
+    "url": "/mathieubouhelier/shopping-online.git/static/css/2.6ca10919.chunk.css"
+  },
+  {
+    "revision": "831c82437b6acb4b1e12",
+    "url": "/mathieubouhelier/shopping-online.git/static/css/main.246f8e45.chunk.css"
+  },
+  {
+    "revision": "9efecb77677f5b68e824",
+    "url": "/mathieubouhelier/shopping-online.git/static/js/2.cdc623c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mathieubouhelier/shopping-online.git/static/js/2.cdc623c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831c82437b6acb4b1e12",
+    "url": "/mathieubouhelier/shopping-online.git/static/js/main.a06d2609.chunk.js"
+  },
+  {
+    "revision": "da8dd96cc4c31255f8d5",
+    "url": "/mathieubouhelier/shopping-online.git/static/js/runtime-main.c43fcbe7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mathieubouhelier/shopping-online.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mathieubouhelier/shopping-online.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mathieubouhelier/shopping-online.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mathieubouhelier/shopping-online.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mathieubouhelier/shopping-online.git/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fdb1cd952ca1072a523ae6dbfdd5bdf9",
+    "url": "/mathieubouhelier/shopping-online.git/static/media/logo.fdb1cd95.png"
+  },
+  {
+    "revision": "2881be2223e21129b46ea022bf86a6ee",
+    "url": "/mathieubouhelier/shopping-online.git/static/media/shop.2881be22.png"
+  }
+]);
